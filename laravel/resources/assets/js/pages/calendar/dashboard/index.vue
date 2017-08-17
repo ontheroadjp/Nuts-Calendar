e31b36f6d68d2b532c97896732f770d59b365966
@ontheroad_jp <dev@ontheroad.jp>
2017-08-17 21:16:35 +0900

    [Add] dashboard layout: html tags

--- laravel/resources/assets/js/pages/calendar/dashboard/index.vue
+++ laravel/resources/assets/js/pages/calendar/dashboard/index.vue
@@ -2,17 +2,75 @@
 <div class="wrapper">
 <div class="container" style="width: 100%; height: 100vh">
 
-<!-- infomation -->
-<div class="columns is-multiline">
-    <div class="column is-4">
-        <date-card></date-card>
+    <!-- infomation -->
+    <div class="columns is-multiline">
+        <div class="column is-4">
+            <date-card></date-card>
+        </div>
     </div>
-</div>
+    
+    <menu-tabs :tabs="tabs"></menu-tabs>
 
-<menu-tabs :tabs="tabs"></menu-tabs>
-<calendar-settings v-show="this.currentId === 1"></calendar-settings>
-<application-settings v-show="this.currentId === 2"></application-settings>
-<account-settings v-show="this.currentId === 3"></account-settings>
+    <div v-show="currentId === 0">
+
+        <div style="margin-bottom: 20px;">
+            <a 
+                :class="['button', theme.primary.class]"
+                @click="clickNewCalendarButton()"
+            >New Calendar</a>
+        </div>
+
+        <!-- user calendars -->
+        <template v-for="uCalendar in userCalendars">
+        <div class="columns is-multiline">
+        <div class="column is-12">
+        
+            <router-link
+                to="/calendar/view"
+                class="title is-4" 
+                @click.native="clickUserCalendar(uCalendar.id)"
+            >
+                <div 
+                    :class="['card', 'is-clickable', theme.primary.class]"
+                    @dragenter="handleDragEnter()"
+                >
+                <div class="card-content" style="height: 120px">
+                <div class="media">
+        
+                    <div class="media-left">
+                        <span class="icon">
+                            <i class="fa fa-calendar"></i>
+                        </span>
+                    </div>
+        
+                    <div class="media-content">
+                        <p style="margin-bottom: 10px;">{{ uCalendar.name }}</p>
+                        <p class="subtitle is-6">
+                            {{ uCalendar.description }}
+                        </p>
+                        <div 
+                            class="icon"
+                            style="
+                                position: absolute;
+                                top: 20px;
+                                right: 20px;
+                            "
+                        ><i class="fa fa-gear"></i></div>
+                    </div>
+        
+                </div><!-- // .media -->
+                </div><!-- // .card-content -->
+                </div><!-- // .card -->
+            </router-link>
+        
+        </div><!-- // .column is-xx -->
+        </div><!-- // .columns -->
+        </template>
+    </div>
+
+    <calendar-settings v-show="currentId === 1"></calendar-settings>
+    <application-settings v-show="currentId === 2"></application-settings>
+    <account-settings v-show="currentId === 3"></account-settings>
 
 </div><!-- // .container -->
 </div><!-- // .wrapper -->
@@ -50,11 +108,45 @@ export default {
     computed: {
         ...mapState({
             currentId: state => state.dashboard.currentId,
+            userCalendars: state => state.calendar.data.userCalendars,
+            theme: state => state.app.theme
         })
     },
+
+    methods: {
+        clickNewCalendarButton: function() {
+            u.clog('New Calendar Button');
+        },
+
+        clickUserCalendar: function(id) {
+            u.clog('changeCalendar(' + id + ')');
+            this.$store.commit('setCurrentCalendarId', id);
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-    /* empty */
+div.card {
+    &.is-koiai.is-clickable:hover {
+        border: 1px solid rgba(25, 96, 182, 0.5);
+    }
+
+    &.is-nadeshiko.is-clickable:hover {
+        border: 1px solid rgba(239, 113, 156, 0.4);
+    }
+
+    &.is-mikan.is-clickable:hover {
+        border: 1px solid rgba(240, 131, 0, 0.4);
+    }
+
+    &.is-sumire.is-clickable:hover {
+        border: 1px solid rgba(112, 88, 163, 0.5);
+    }
+
+    &.is-moegi.is-clickable:hover {
+        border: 1px solid rgba(170, 207, 83, 0.5);
+    }
+}
+
 </style>
