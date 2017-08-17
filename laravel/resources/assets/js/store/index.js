b7e3b9e7b015e18f4d20e3664d3502f9dab9d4aa
@ontheroad_jp <dev@ontheroad.jp>
2017-08-17 12:17:38 +0900

    [Refactor] clean

--- laravel/resources/assets/js/store/index.js
+++ laravel/resources/assets/js/store/index.js
@@ -1,26 +1,24 @@
-import Vue from 'vue';
+import Vue  from 'vue';
 import Vuex from 'vuex';
-import state from './state.js';
-import mutations from './mutations.js';
-import actions from './actions.js';
-import getters from './getters.js';
+import state        from './state.js';
+import mutations    from './mutations.js';
+import actions      from './actions.js';
+import getters      from './getters.js';
 
 //import appTheme from './app/theme.js';
 //import appNotification from './app/notification.js';
 
-import { en } from '../i18n/en.js';
-import ja from '../i18n/ja.js';
+import { en }   from '../i18n/en.js';
+import ja       from '../i18n/ja.js';
 
 import tableViewToolPalette from './calendar/table-view/tool-palette.js';
-
-import columnInsert from './calendar/table-view/column/insert.js';
-import columnUpdate from './calendar/table-view/column/update.js';
-import columnRemove from './calendar/table-view/column/remove.js';
-
-import itemInsert from './calendar/table-view/item/insert.js';
-import itemUpdate from './calendar/table-view/item/update.js';
-import itemRemove from './calendar/table-view/item/remove.js';
-import itemDnd from './calendar/table-view/item/dnd.js';
+import columnInsert         from './calendar/table-view/column/insert.js';
+import columnUpdate         from './calendar/table-view/column/update.js';
+import columnRemove         from './calendar/table-view/column/remove.js';
+import itemInsert           from './calendar/table-view/item/insert.js';
+import itemUpdate           from './calendar/table-view/item/update.js';
+import itemRemove           from './calendar/table-view/item/remove.js';
+import itemDnd              from './calendar/table-view/item/dnd.js';
 
 Vue.use(Vuex);
 
@@ -59,6 +57,13 @@ export default new Vuex.Store({
         action: {
             namespaced: true,
             modules: {
+                dashboard: {
+                    namespaced: true,
+                    modules: {
+
+                    }
+                },
+
                 calendar: {
                     namespaced: true,
                     modules: {
