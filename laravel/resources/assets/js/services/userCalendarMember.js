c64abc0087e730dca230aa0732fae6dbe03b7d25
@ontheroad_jp <dev@ontheroad.jp>
2017-08-28 05:37:06 +0900

    [Refactor] new to add: addMember() and removeMember()

--- laravel/resources/assets/js/services/userCalendarMember.js
+++ laravel/resources/assets/js/services/userCalendarMember.js
@@ -5,48 +5,42 @@ export default {
     },
 
     methods: {
-        update: function(userCalendarId, name, description) {
-            u.clog('update()');
-            http.fetchPut( '/api/v1/calendar/' + userCalendarId, {
-                'name': name,
-                'description': description
-            }) 
+        chengeMember: function(userCalendarId, memberId, value) {
+            u.clog('checkMember()');
+
+            const url =  '/api/v1/calendar/member';
+            const data = {
+                'user_calendar_id': userCalendarId,
+                'member_id': memberId
+            }
+
+            if( value ) {
+                this.addMember(url, data);
+
+            } else {
+                this.removeMember(url, data);
+
+            }
+        },
+
+        addMember: function(url, data) {
+            http.fetchPost(url, data)
                 .then( response => {
-                    u.clog("success");
+                    u.clog("success: ['member_id' => " + response.data.member_id + "]");
                 })
                 .catch( error => {
                     u.clog('error: ' + error.response.status);
                 });
         },
 
-        chengeMember: function(userCalendarId, memberId, value) {
-            u.clog('checkMember()');
-
-            if( value ) {
-                http.fetchPost( '/api/v1/calendar/member', {
-                    'user_calendar_id': userCalendarId,
-                    'member_id': memberId
-                }) 
-                    .then( response => {
-                        u.clog("success: ['member_id' => " + response.data.member_id + "]");
-                    })
-                    .catch( error => {
-                        u.clog('error: ' + error.response.status);
-                    });
-
-            } else {
-                http.fetchDelete( '/api/v1/calendar/member', {
-                    'user_calendar_id': userCalendarId,
-                    'member_id': memberId
+        removeMember: function(url, data) {
+            http.fetchDelete( url, data)
+                .then( response => {
+                    u.clog('success: ' + response.data + ' record(s) removed.');
                 })
-                    .then( response => {
-                        u.clog('success: ' + response.data + ' record(s) removed.');
-                    })
-                    .catch( error => {
-                        u.clog('error: ' + error.response.status);
-                    });
-
-            }
+                .catch( error => {
+                    u.clog('error: ' + error.response.status);
+                });
         }
 
     }
