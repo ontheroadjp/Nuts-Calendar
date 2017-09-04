7124ab44e932558f3d69a6aaa4d70ac0821f018c
@ontheroad_jp <dev@ontheroad.jp>
2017-09-04 22:27:39 +0900

    [Clean] codes commented out

--- laravel/resources/assets/js/store/modules/member/remove.js
+++ laravel/resources/assets/js/store/modules/member/remove.js
@@ -4,29 +4,14 @@ export default {
     namespaced: true,
 
     state: {
-//        isActive: false,
         isLoading: false,
-//        calendars: '',
-//        members: '',
-//        deletingMemberId: ''
     },
 
     actions: {
-//        prepare( { commit, rootState }, { deletingMember }  ) {
-//            const calendars = rootState.calendar.data.calendars;
-//            const members = rootState.calendar.data.members;
-//            commit('prepare', { calendars, members, deletingMember } );
-//        },
-
-//        prepare( { commit, rootState }, { deletingMemberId }  ) {
-//            commit('prepare', { deletingMemberId } );
-//        },
-
         remove( { commit, state, rootState }, { index } ) {
             u.clog('remove()');
             commit('start');
 
-//            const calendars = rootState.calendar.data.calendars;
             const members = rootState.member.data.members;
 
             const url = '/api/v1/member/' + members[index].id;
@@ -62,35 +47,16 @@ export default {
     },
 
     mutations: {
-//        prepare( state, { calendars, members, deletingMember } ) {
-//            state.calendars = calendars,
-//            state.members = members,
-//            state.deletingMember = deletingMember
-//            state.isActive = true;
-//        },
-
-//        prepare( state, { deletingMemberId } ) {
-//            state.deletingMemberId = deletingMemberId
-//            state.isActive = true;
-//        },
-
         start( state ) {
             state.isLoading = true;
         },
 
         remove( state, { members, index } ) {
             Vue.delete(members, index);
-//            state.calendars.forEach( function(val, index) {
-//                Vue.delete( val.items, state.deletingMemberId);
-//            });
         },
 
         reset( state ) {
-//            state.isActive = false;
             state.isLoading = false;
-//            state.calendars = '';
-//            state.members = '';
-//            state.deletingMemberId = '';
         }
     }
 }
