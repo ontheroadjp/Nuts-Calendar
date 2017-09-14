0ee9d9378ad5df670a8b0842957b339b97eec1fa
@ontheroad_jp <dev@ontheroad.jp>
2017-09-14 12:53:30 +0900

    [Clean] remove some codes commented out

--- laravel/resources/assets/js/components/black-screen.vue
+++ laravel/resources/assets/js/components/black-screen.vue
@@ -1,12 +1,11 @@
 <template>
-<!--
-    <div class="black-screen" :style="`background-color: ${color}; width: ${width}px`">
-        <slot></slot>
-    </div>
--->
-    <div class="black-screen" :style="`background-color: ${color};`" @click="onClick">
-        <slot></slot>
-    </div>
+<div 
+    class="black-screen" 
+    :style="`background-color: ${color};`" 
+    @click="onClick"
+>
+    <slot></slot>
+</div>
 </template>
 
 <script>
@@ -14,24 +13,7 @@ export default {
     props: {
         color:   { type: String, default: 'rgba(217, 217, 217, 0.75)' },
         onClick: { type: Function, default: () => {} }
-//        overlayId: { type: String, default: '' }
-    },
-
-//    data() {
-//        return {
-//            width: 0
-//        }
-//    },
-
-//    mounted() {
-//        if( this.overlayId === '' ) return;
-//
-//        let target = window.document.getElementById(this.overlayId);
-//
-//        this.width = target.scrollWidth;
-//    },
-
-
+    }
 }
 </script>
 
