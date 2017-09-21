3e77aa6fafb522b90523870aab5b03eb70134b3c
@ontheroad_jp <dev@ontheroad.jp>
2017-09-21 21:23:48 +0900

    [Clean] some codes commented out

--- laravel/resources/assets/js/pages/dashboard/user-calendar-modal.vue
+++ laravel/resources/assets/js/pages/dashboard/user-calendar-modal.vue
@@ -7,7 +7,6 @@
 </template>
 
 <script>
-import { mapState, mapActions, mapMutations } from 'vuex';
 import simpleModal from '../../components/simple-modal.vue';
 import userCalendarModalContent from './user-calendar-modal-content.vue';
 
@@ -22,73 +21,10 @@ export default {
         isActive:     { type: Boolean, required: true }
     },
 
-//    data() {
-//        return {
-//            editingId: '',
-//        }
-//    },
-
-//    computed: {
-////        ...mapState({
-////            theme: state => state.app.theme
-////        }),
-//
-////        ...mapState('userCalendar', {
-////            updateState: state => state.update
-////        }),
-//
-////        ...mapState('member', {
-////            members: state => state.data.members
-////        }),
-//
-////        ...mapState('userCalendarMember', {
-////            userCalendarMembers: state => state.data.userCalendarMembers
-////        })
-//    },
-
     methods: {
-//        ...mapMutations('userCalendar/update', {
-//            setUpdateValue: 'setUpdateValue'
-//        }),
-
-//        ...mapActions('userCalendar/update', {
-////            updateName: 'updateName',
-////            updateDescription: 'updateDescription'
-//        }),
-
         clickClose: function() {
             this.$emit('update:isActive', false);
-        },
-
-//        clickSaveName: function() {
-//            u.clog('clickSave()');
-//            this.updateName(this.userCalendar.id);
-//        },
-
-//        clickSaveDescription: function() {
-//            u.clog('clickSave()');
-//            this.updateDescription(this.userCalendar.id);
-//        },
-
-//        clickUndo: function() {
-//            this.input.name = this.userCalendar.name;
-//            this.input.description = this.userCalendar.description;
-//        }
+        }
     }
 };
 </script>
-
-<style lang="scss" scoped>
-/*
-.members {
-    margin: 10px 20px;
-}
-.member {
-    line-height: 2.0rem;
-}
-
-.member-checkbox-label {
-    margin: 0 5px;
-}
-*/
-</style>
