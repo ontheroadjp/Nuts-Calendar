80e68b475e96c0331a2877666a8f9ec97a8dd9ef
@ontheroad_jp <dev@ontheroad.jp>
2017-09-21 21:26:24 +0900

    [Refactor] minor issue

--- laravel/resources/assets/js/pages/dashboard/user-calendar-card.vue
+++ laravel/resources/assets/js/pages/dashboard/user-calendar-card.vue
@@ -1,6 +1,10 @@
 <template>
 <div>
-    <user-calendar-modal :isActive.sync="modal.isActive" :userCalendar="userCalendar"></user-calendar-modal>
+    <user-calendar-modal
+        :isActive.sync="modal.isActive"
+        :userCalendar="userCalendar"
+    ></user-calendar-modal>
+
     <div :class="['card', 'is-clickable', theme.primary.class]"
         :style="style.calendarCard">
 
@@ -12,28 +16,28 @@
                     <i class="fa fa-calendar"></i>
                 </span>
             </div>
-    
+
             <div class="media-content">
                 <p style="margin-bottom: 10px;">
                     <router-link
                         to="/calendar/view"
-                        class="title thin-200" 
+                        class="title thin-200"
                         @click.native="clickUserCalendar(userCalendar.id)"
                     >{{ userCalendar.name }}
                     </router-link>
                 </p>
-    
+
                 <p class="subtitle thin">
                     {{ userCalendar.description }}
                 </p>
-    
+
                 <a @click="openDialog(userCalendar)" style="transition: color 0.3s">
                     <div class="icon" style="position: absolute; top: 6px; right: 3px; color: #fff">
                         <i class="fa fa-gear" style="margin-right: 5px"></i>
                     </div>
                 </a>
             </div>
-    
+
         </div><!-- // .media -->
         </div><!-- // .card-content -->
     </div><!-- // .card -->
