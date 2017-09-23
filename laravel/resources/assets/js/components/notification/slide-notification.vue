396a02d92f2ea4c47d71daed878bf8f11a711b40
@ontheroad_jp <dev@ontheroad.jp>
2017-09-23 18:17:51 +0900

    [Refactor] replace some inline styles to class

--- laravel/resources/assets/js/components/notification/slide-notification.vue
+++ laravel/resources/assets/js/components/notification/slide-notification.vue
@@ -1,15 +1,18 @@
 <template>
 <slide-panel v-if="isActive" :bgColor="bgColor" :height="height">
-    <div style="display: flex; justify-content: center; align-items: center; height: 75%;">
+    <div class="panel-body">
 <!--
+    <div style="display: flex; justify-content: center; align-items: center; height: 75%;">
         <i v-if="type === 'success'" class="fa fa-check-circle fa-3x"></i>
         <i v-else-if="type === 'danger' || type === 'failed'" class="fa fa-exclamation-circle fa-3x"></i>
 -->
         <icon :type="type" :size=3></icon>
-        <div style="padding-left: 10px;" v-text="message ? message : defaultMessage"></div>
+        <div style="padding-left: 10px;"
+            v-text="message ? message : defaultMessage"
+        ></div>
     </div>
 
-    <div class="notification-buttons">
+    <div class="buttons">
         <button class="button strip"
             style="color:#fff"
             @click="clickClose()"
@@ -62,7 +65,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.notification-buttons {
+.panel-body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 75%;
+}
+
+.buttons {
     display: inline-flex;
     justify-content: space-around;
     width: 100%;
