44740d33386a69d1c4415cb6a53e38b98aceb52f
@ontheroad_jp <dev@ontheroad.jp>
2017-09-23 17:05:35 +0900

    [Refactor] minor issue

--- laravel/resources/assets/js/pages/dashboard/user-calendars/notification.vue
+++ laravel/resources/assets/js/pages/dashboard/user-calendars/notification.vue
@@ -1,9 +1,9 @@
 <template>
 <slide-panel v-if="isActive" :bgColor="bgColor" :height="height">
     <div style="display: flex; justify-content: center; align-items: center; height: 75%;">
-            <i v-if="type === 'success'" class="fa fa-check-circle fa-3x"></i>
-            <i v-else-if="type === 'danger'" class="fa fa-exclamation-circle fa-3x"></i>
-        <div style="padding-left: 10px;">{{ message }}</div>
+        <i v-if="type === 'success'" class="fa fa-check-circle fa-3x"></i>
+        <i v-else-if="type === 'danger'" class="fa fa-exclamation-circle fa-3x"></i>
+        <div style="padding-left: 10px;" v-text="message ? message : defaultMessage"></div>
     </div>
 
     <div class="notification-buttons">
@@ -22,33 +22,29 @@ export default {
     components: { slidePanel },
 
     props: {
-        type:            { type: String, required: true, validator: function(value) {
-                                const expectation = [
-                                    'success', 'info', 'warning', 'danger'
-                                ];
-                                return expectation.indexOf(value) > -1;
-                         }},
-        height:          { type: String, default: '300px' },
-        isActive:        { type: Boolean, required: true },
-        message:         { type: String, default: '' }
+        type:     { type: String, required: true, validator: function(value) {
+                         const expectation = [
+                             'success', 'info', 'warning', 'danger'
+                         ];
+                         return expectation.indexOf(value) > -1;
+                  }},
+        height:   { type: String, default: '300px' },
+        isActive: { type: Boolean, required: true },
+        message:  { type: String, required: false }
     },
 
     computed: {
         bgColor: function() {
             switch(this.type) {
-                case 'success':
-                    return '#23d160';
-                    break;
-                case 'info':
-                    return '#3273dc';
-                    break;
-                case 'warning':
-                    return '#ffdd57';
-                    break;
-                case 'danger':
-                    return '#ff3860';
-                    break;
+                case "success": return '#23d160'; break;
+                case "info":    return '#3273dc'; break;
+                case "warning": return '#ffdd57'; break;
+                case "danger":  return '#ff3860'; break;
             }
+        },
+
+        defaultMessage: function() {
+            return this.type.toUpperCase();
         }
     },
 
