01a46542f58d2f682625725eea242a2a22d25fe0
@ontheroad_jp <dev@ontheroad.jp>
2017-09-23 13:57:42 +0900

    [New] slide-panel component

---
+++ laravel/resources/assets/js/components/slide-panel.vue
@@ -0,0 +1,57 @@
+<template>
+    <transition name="slide-panel">
+        <div class="card slide-panel">
+            <slot></slot>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    props: {
+        bgColor:  { type: String, default: '#e6e6e6' },
+        height:   { type: String, default: '300px' }
+    },
+
+    mounted: function() {
+        const doc = window.document;
+        const css = doc.createElement('style');
+        const rule = document.createTextNode(`
+            .slide-panel {
+                background-color: ${this.bgColor};
+                padding: 10px;
+                color: #fff;
+                text-align: center;
+                overflow: hidden;
+                height: ${this.height};
+                display: flex;
+                flex-flow: column nowrap;
+                justify-content: space-between;
+                z-index: 99;
+            }
+
+            .slide-panel-enter-to,
+            .slide-panel-leave {
+                opacity: 1;
+                height: ${this.height};
+            }
+
+            .slide-panel-enter-active,
+            .slide-panel-leave-active {
+                transition: all .3s ease;
+            }
+
+            .slide-panel-leave-to,
+            .slide-panel-enter {
+                height: 0;
+                opacity: 0;
+            }
+        `);
+
+        css.id = 'slide-panel';
+        css.type = 'text/css';
+        css.appendChild(rule);
+        doc.getElementsByTagName('head')[0].appendChild(css);
+    }
+};
+</script>
--- laravel/resources/assets/js/pages/dashboard/user-calendars/notification.vue
+++ laravel/resources/assets/js/pages/dashboard/user-calendars/notification.vue
@@ -1,79 +1,75 @@
 <template>
-    <transition name="notification">
-        <div class="card notification"
-            :style="isSuccessActive
-                        ? 'background-color: #23d160'
-                        : 'background-color: #ff3860'
-            "
-            v-show="isSuccessActive || isFailedActive"
-        >
-            <div style="
-                display: flex;
-                justify-content: center;
-                height: 75%;
-            ">
-                <div style="display: flex; justify-content: space-around; align-items: center;">
-                    <i v-if="isSuccessActive" class="fa fa-check-circle fa-3x"></i>
-                    <i v-else-if="isFailedActive" class="fa fa-exclamation-circle fa-3x"></i>
-                </div>
-                <div v-text="isSuccessActive ? 'Success' : 'Failed'"
-            </div>
-
-            <div class="notification-buttons">
-                <button class="button strip"
-                    style="color:#fff"
-                    @click="clickClose()"
-                >OK</button>
-            </div>
+<slide-panel v-if="isSuccessActive || isFailedActive" :bgColor="bgColor" :height="height">
+    <div style="display: flex; justify-content: center; height: 75%;">
+        <div style="display: flex; justify-content: space-around; align-items: center;">
+            <i v-if="isSuccessActive" class="fa fa-check-circle fa-3x"></i>
+            <i v-else-if="isFailedActive" class="fa fa-exclamation-circle fa-3x"></i>
         </div>
-    </transition>
+        <div v-text="isSuccessActive ? 'Success' : 'Failed'"></div>
+    </div>
+
+    <div class="notification-buttons">
+        <button class="button strip"
+            style="color:#fff"
+            @click="clickClose()"
+        >OK</button>
+    </div>
+</slide-panel>
 </template>
 
 <script>
+import slidePanel from '../../../components/slide-panel.vue';
+
 export default {
+    components: { slidePanel },
+
     props: {
-        isSuccessActive: { type: Boolean, required: true },
-        isFailedActive:  { type: Boolean, required: true },
+        isSuccessActive: { type: Boolean, default: false },
+        isFailedActive:  { type: Boolean, default: false },
         height:          { type: String, default: '300px' }
+    },
+
+    computed: {
+        bgColor: function() {
+            return this.isSuccessActive ? '#23d160' : '#ff3860';
+        }
     },
 
     methods: {
         clickClose() {
             this.$emit('close');
-        },
-
+        }
     },
 
-    mounted: function() {
-        const doc = window.document;
-        const css = doc.createElement('style');
-        const rule = document.createTextNode(`
-            .notification {
-                background-color: red;
-                padding: 10px;
-                color: #fff;
-                text-align: center;
-                overflow: hidden;
-                height: ${this.height};
-                display: flex;
-                flex-flow: column nowrap;
-                justify-content: space-between;
-                z-index: 99;
-            }
-
-            .notification-enter-to,
-            .notification-leave {
-                opacity: 1;
-                height: ${this.height};
-            }
-        `);
-
-        css.id = 'delete-confirm';
-        css.type = 'text/css';
-        css.appendChild(rule);
-        doc.getElementsByTagName('head')[0].appendChild(css);
-    }
-
+//    mounted: function() {
+//        const doc = window.document;
+//        const css = doc.createElement('style');
+//        const rule = document.createTextNode(`
+//            .notification {
+//                background-color: red;
+//                padding: 10px;
+//                color: #fff;
+//                text-align: center;
+//                overflow: hidden;
+//                height: ${this.height};
+//                display: flex;
+//                flex-flow: column nowrap;
+//                justify-content: space-between;
+//                z-index: 99;
+//            }
+//
+//            .notification-enter-to,
+//            .notification-leave {
+//                opacity: 1;
+//                height: ${this.height};
+//            }
+//        `);
+//
+//        css.id = 'delete-confirm';
+//        css.type = 'text/css';
+//        css.appendChild(rule);
+//        doc.getElementsByTagName('head')[0].appendChild(css);
+//    }
 };
 </script>
 
@@ -83,7 +79,7 @@ export default {
     justify-content: space-around;
     width: 100%;
 }
-
+/*
 .notification-enter-active,
 .notification-leave-active {
     transition: all .3s ease;
@@ -94,4 +90,5 @@ export default {
     height: 0;
     opacity: 0;
 }
+*/
 </style>
