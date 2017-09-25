846f78bd59f29447544b03e0bea2a4ed08deaa40
@ontheroad_jp <dev@ontheroad.jp>
2017-09-25 23:35:34 +0900

    [Update] add eleven assertions

--- laravel/resources/assets/js/test/unit/specs/text-input.spec.js
+++ laravel/resources/assets/js/test/unit/specs/text-input.spec.js
@@ -2,13 +2,73 @@ import textInput from '../../../src/components/form/text-input.vue';
 import { mount } from 'avoriaz';
 
 describe('text-input.vue', () => {
-    it('has error props:minTextValue value is 10 and initialValue is hoge', () => {
-        const wrapper = mount(textInput, {
-            propsData: {
-                initialValue: 'hoge',
-                minTextValue: 10
-            }
-        });
-        expect(wrapper.instance().errorResult()).to.be.eql(false);
+    const wrapper = mount(textInput, {
+        propsData: {
+            id: 'input-id',
+            initialValue: 'hoge',
+            minTextLength: 10,
+            maxTextLength: 20
+        }
     });
+
+    describe('when component mounted', () => {
+        it('initialValue should equal to \'hoge\'', () => {
+            expect(wrapper.vm.initialValue).to.be.eql('hoge');
+        });
+
+        it('input.value should equal to initialValue', () => {
+            expect(wrapper.data().input.value).to.be.eql(wrapper.vm.initialValue);
+        });
+
+        it('form value should equal to input.value', () => {
+            const input = wrapper.find('input#input-id')[0]
+            expect(input.element.value).to.be.eql(wrapper.data().input.value);
+        });
+    });
+
+    describe('when form value changed to \'foo\'', () => {
+        it('input.value should equal to \'foo\'', () => {
+            const input = wrapper.find('input#input-id')[0];
+            input.element.value = 'foo';
+            input.trigger('input');
+            expect(wrapper.data().input.value).to.be.eql('foo');
+        });
+
+        it('form value should not equal to initialValue', () => {
+            const input = wrapper.find('input#input-id')[0];
+            expect(input.element.value).to.be.eql('foo');
+            expect(wrapper.vm.initialValue).to.be.eql('hoge');
+        });
+    });
+
+    describe('errorResult', () => {
+        it('input.value < minTextLength ', () => {
+            expect(wrapper.vm.errorResult).to.be.eql(true)
+        });
+
+        it('input.value > minTextLength', () => {
+            wrapper.data().input.value = 'hogehogehoge';
+            expect(wrapper.vm.errorResult).to.be.eql(false);
+        });
+
+        it('input.value < maxTextLength', () => {
+            expect(wrapper.vm.errorResult).to.be.eql(false);
+        });
+
+        it('input.value > maxTextLength', () => {
+            wrapper.data().input.value = 'hogehogehogehogehogehoge';
+            expect(wrapper.vm.errorResult).to.be.eql(true);
+        });
+
+    });
+
+    describe('when fire blur event on form fireEvent() called onece', () => {
+        it('blur event on form', () => {
+            const fireEvent = sinon.spy(wrapper.vm, 'fireEvent');
+            wrapper.find('input#input-id')[0].trigger('blur');
+            expect(typeof wrapper.vm.fireEvent).to.be.eql('function');
+            expect(fireEvent).to.be.calledOnce;
+        });
+    });
+
 });
