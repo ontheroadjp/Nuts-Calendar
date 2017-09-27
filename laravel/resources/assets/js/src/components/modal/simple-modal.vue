a6a5e6b61bede38c10b3589bcf0dca7e763b93ab
@ontheroad_jp <dev@ontheroad.jp>
2017-09-27 13:53:15 +0900

    [Clean] remove some codes commented out

--- laravel/resources/assets/js/src/components/modal/simple-modal.vue
+++ laravel/resources/assets/js/src/components/modal/simple-modal.vue
@@ -1,22 +1,4 @@
 <template>
-<!--
-<modal :blackScreenColor="blackScreenColor" :isActive="isActive">
-    <div class="modal-card">
-
-        <button class="modal-close is-large" aria-label="close"
-            style="position: absolute;
-                    top: 20px;
-                    right: 20px;
-                    background-color: rgba(84, 110, 122, 0.5);
-                    z-index: 1;
-            "
-            @click="onClose()"
-        ></button>
-
-        <slot></slot>
-    </div>
-</modal>
--->
 <transition name="modal-fade">
     <div class="modal is-active" v-if="isActive">
         <div class="modal-background"
@@ -40,10 +22,8 @@
 </template>
 
 <script>
-//import modal from './modal.vue';
 
 export default {
-//    components: { modal },
     props: {
         blackScreenColor: { type: String, default: 'rgba(10,10,10,0.85)' },
         isActive: { type: Boolean, required: true },
