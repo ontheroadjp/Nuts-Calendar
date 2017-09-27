62a5c1c9acd67a78732a703673ab189be1812582
@ontheroad_jp <dev@ontheroad.jp>
2017-09-27 09:55:28 +0900

    [Modify] minor issue

--- laravel/resources/assets/js/test/unit/specs/components/slide-panel/slide-panel.spec.js
+++ laravel/resources/assets/js/test/unit/specs/components/slide-panel/slide-panel.spec.js
@@ -1,15 +1,20 @@
 import slidePanel from '../../../../../src/components/slide-panel/slide-panel.vue';
+import simpleSlidePanel from '../../../../../src/components/slide-panel/simple-slide-panel.vue';
 import { mount } from 'avoriaz';
 
 describe('components/slide-panel/slide-panel.vue', () => {
-    const wrapper = mount(slidePanel, {
-        propsData: {
-            height: '600px',
-            duration: '1s'
-        }
-    });
+    const propsData = {
+        height: '600px',
+        duration: '1s'
+    };
+
+    const slots = {
+        default: simpleSlidePanel
+    };
 
     describe('when component mounted', () => {
+        const wrapper = mount(slidePanel, { propsData, slots });
+
         it('height should equal to 600px', () => {
             expect(wrapper.vm.height).to.be.eql('600px');
         });
