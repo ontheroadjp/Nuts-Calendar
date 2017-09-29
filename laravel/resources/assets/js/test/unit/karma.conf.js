a1430cb5a7c3698cf13e312da7624a3175bdc460
@ontheroad_jp <dev@ontheroad.jp>
2017-09-29 22:20:58 +0900

    [Add] several browser

--- laravel/resources/assets/js/test/unit/karma.conf.js
+++ laravel/resources/assets/js/test/unit/karma.conf.js
@@ -1,17 +1,13 @@
-// This is a karma config file. For more details see
-//   http://karma-runner.github.io/0.13/config/configuration-file.html
-// we are also using it with karma-webpack
-//   https://github.com/webpack/karma-webpack
-
 var webpackConfig = require('../../build/webpack.test.conf')
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
-    // to run in additional browsers:
-    // 1. install corresponding karma launcher
-    //    http://karma-runner.github.io/0.13/config/browsers.html
-    // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
+//    browsers: ['PhantomJS', 'ChromeHeadless', 'Firefox'],
+//    browsers: ['PhantomJS'],
+//    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+//    browsers: ['Firefox'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: [
@@ -25,6 +21,8 @@ module.exports = function (config) {
     webpackMiddleware: {
       noInfo: true
     },
+//    logLevel: config.LOG_INFO,
+    logLevel: config.DEBUG,
     coverageReporter: {
       dir: './coverage',
       reporters: [
