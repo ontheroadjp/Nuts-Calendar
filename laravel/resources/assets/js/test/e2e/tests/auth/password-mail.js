7576c7efb9cb0614b627ca77fdba8d9ad7eda5eb
@ontheroad_jp <dev@ontheroad.jp>
2017-10-05 20:53:53 +0900

    [Refactor] minor issue

--- laravel/resources/assets/js/test/e2e/tests/auth/password-mail.js
+++ laravel/resources/assets/js/test/e2e/tests/auth/password-mail.js
@@ -1,5 +1,3 @@
-const axios = require('axios');
-
 const changePassword = function(client, emailAddress, newPassword) {
     client.httpDelete('http://localhost:1080/messages', (response) => {
         const sendMailPage = client.page.auth['password-email']();
@@ -9,50 +7,50 @@ const changePassword = function(client, emailAddress, newPassword) {
 
         sendMailPage.navigate()
             .sendMail(emailAddress)
-            .assert.containsText('span#result-message', 'Sent Reset Link Mail Successfully');
+            .assert.containsText('span#result-message', 'Sent Reset Link Mail Successfully')
+            .pause(1000)
+            .httpGet('http://localhost:1080/messages/1.source', (response) => {
+                const mailArray = response.data.split(/\r\n|\r|\n/);
+                let mailData = {}
+                mailArray.forEach((line) => {
+                    if(line.indexOf(': ') === -1) return;
+                    let key = line.split(':')[0].replace('-','').trim();
+                    let value = line.split(':')[1].trim();
+                    mailData[key] = value;
+                });
 
-        client.pause(1000);
+                client.expect(mailData.From === 'admin <admin@familycal.com>').to.be.eql(true);
+                client.expect(mailData.To === emailAddress).to.be.eql(true);
+                client.expect(mailData.Subject === 'Your Password Reset Link').to.be.eql(true);
+                client.expect(mailData.ContentType === 'text/html; charset=utf-8').to.be.eql(true);
 
-        // fetch password reset link email
-        client.httpGet('http://localhost:1080/messages/1.source', (response) => {
-            const mailArray = response.data.split(/\r\n|\r|\n/);
-            let mailData = {}
-            mailArray.forEach((line) => {
-                if(line.indexOf(': ') === -1) return;
-                let key = line.split(':')[0].replace('-','').trim();
-                let value = line.split(':')[1].trim();
-                mailData[key] = value;
-            });
+                // move to password reset page (click text in an email)
+                client
+                    .url(client.launch_url + '/password/reset/' + mailData.XToken)
+                    .pause(3000)
 
-            client.expect(mailData.From === 'admin <admin@familycal.com>').to.be.eql(true);
-            client.expect(mailData.To === emailAddress).to.be.eql(true);
-            client.expect(mailData.Subject === 'Your Password Reset Link').to.be.eql(true);
-            client.expect(mailData.ContentType === 'text/html; charset=utf-8').to.be.eql(true);
+                // change password
+                resetPage
+                    .resetPassword(emailAddress, newPassword, 3000)
+                    .assert.containsText('div#password-reset-result', 'Password reset successfully');
 
-            // move to password reset page (click text in an email)
-            client
-                .url(client.launch_url + '/password/reset/' + mailData.XToken)
-                .pause(3000);
+                // sign-in with new password
+                loginPage
+                    .navigate()
+                    .signin(emailAddress, newPassword)
+                    .assert.urlEquals(client.launch_url + '/dashboard')
+                    .assert.elementNotPresent('a[href=/login]')
+                    .assert.elementPresent('div.container#dashboard')
+                    .pause(3000)
 
-            // change password
-            resetPage
-                .resetPassword(emailAddress, newPassword, 3000)
-                .assert.containsText('div#password-reset-result', 'Password reset successfully');
-
-            // sign-in with new password
-            loginPage.navigate()
-                .signin(emailAddress, newPassword)
-                .assert.urlEquals(client.launch_url + '/dashboard')
-                .assert.elementNotPresent('a[href=/login]')
-                .assert.elementPresent('div.container#dashboard')
-                .pause(3000)
-
-            navbar
-                .signout();
+                // sign-out
+                navbar
+                    .signout();
 
         }, (error) => {
             console.log('Fetch mails from Mailcatcher: ' + error);
         });
+
     }, (error) => {
         console.log('Delete all emails on MailCatcher: ' + error);
     });
