59e01306de95c3abd7643c43e76f91e6838d41c1
@ontheroad_jp <dev@ontheroad.jp>
2017-10-10 12:36:58 +0900

    [Add] max_calendars, max_members and max_shared_members property

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('max_calendars')->default(2);
+            $table->integer('max_members')->default(10);
+            $table->integer('max_shared_members')->default(1);
 //            $table->char('api_token', 60)->unique();
             $table->rememberToken();
             $table->timestamps();
