6414cf3aaa5d66713bedd7b02233ff5c305a27c0
@ontheroad_jp <dev@ontheroad.jp>
2017-10-12 11:14:32 +0900

    [Update] integrate mutation-types

--- laravel/resources/assets/js/src/store/mutation-types.js
+++ laravel/resources/assets/js/src/store/mutation-types.js
@@ -1,31 +1,43 @@
 // --------------------------------------------
 // notification
 // --------------------------------------------
-export const NOTIFY_PRIMARY = 'NOTIFY_PRIMARY'
-export const NOTIFY_SUCCESS = 'NOTIFY_SUCCESS'
-export const NOTIFY_INFO = 'NOTIFY_INFO'
-export const NOTIFY_WARNING = 'NOTIFY_WARNING'
-export const NOTIFY_DANGER = 'NOTIFY_DANGER'
-export const SET_NOTIFICATION = 'SET_NOTIFICATION'
-export const RESET_NOTIFICATION = 'RESET_NOTIFICATION'
+export const NOTIFY_PRIMARY = 'NOTIFY_PRIMARY';
+export const NOTIFY_SUCCESS = 'NOTIFY_SUCCESS';
+export const NOTIFY_INFO = 'NOTIFY_INFO';
+export const NOTIFY_WARNING = 'NOTIFY_WARNING';
+export const NOTIFY_DANGER = 'NOTIFY_DANGER';
+export const SET_NOTIFICATION = 'SET_NOTIFICATION';
+export const RESET_NOTIFICATION = 'RESET_NOTIFICATION';
 
 // --------------------------------------------
 // modules
 // --------------------------------------------
+// calendar
+//export const INIT = 'INIT';
+export const SET_VALUE = 'SET_VALUE';
+//export const IS_LOADING = 'IS_LOADING';
+
+// tableView
+export const SORT_CELL_ITEMS = 'SORT_CELL_ITEMS';
+export const CHECK_TIME = 'CHECK_TIME';
+
+// --------------------------------------------
+// modules (store)
+// --------------------------------------------
 // index
-export const INIT = 'INIT'
-export const ADD = 'ADD'
-export const REMOVE = 'REMOVE'
+export const INIT = 'INIT';
+export const ADD = 'ADD';
+export const REMOVE = 'REMOVE';
 
 // insert, update, remove
-export const IS_LOADING = 'IS_LOADING'
+export const IS_LOADING = 'IS_LOADING';
 
 // insert, update
-export const RESET = 'RESET'
+export const RESET = 'RESET';
 
 // insert
-export const SET_INSERT_VALUE = 'SET_INSERT_VALUE'
+export const SET_INSERT_VALUE = 'SET_INSERT_VALUE';
 
 // update
 export const PREPARE = 'PREPARE';
-export const SET_UPDATE_VALUE = 'SET_UPDATE_VALUE'
+export const SET_UPDATE_VALUE = 'SET_UPDATE_VALUE';
