cd6689472fe94aa908cd4a0fbaea1a6aecc76184
@ontheroad_jp <dev@ontheroad.jp>
2017-12-18 12:01:07 +0900

    [Modify] minor change

--- laravel/resources/assets/js/src/pages/root.vue
+++ laravel/resources/assets/js/src/pages/root.vue
@@ -2,22 +2,48 @@
 <div>
     <section :class="['hero', theme.primary.class, 'is-fullheight']">
     <div class="hero-body">
-    <div id="home" class="container">
+    <div id="home" class="container" style="display: flex;">
 
-        <div class="contents" style="width: 40%;">
-            <h1 class="title thin-300" style="color:#fff">Nuts <strong>Calendar</strong>(BETA)</h1>
-            <div style="margin-bottom: 30px;">
-                <h2 class="subtitle thin" style="color:#fff">
-                    Grasp the schedules of your own multiple roles,
-                    family members, lovers, and colleagues with one
-                    simple calendar !!
-                </h2>
+        <div class="main-hero-left">
+            <div class="contents" style="width: 60%;">
+                <img src="images/nuts-logo-big.png" />
+                <small>(BETA)</small>
+
+                <div style="margin-bottom: 30px;">
+                    <h2 class="subtitle thin" style="color:#fff">
+                        Grasp the schedules of your own multiple roles,
+                        family members, lovers, and colleagues with one
+                        simple calendar !!
+                    </h2>
+                </div>
+
+                <div style="display: flex; justify-content: space-around;">
+                    <router-link
+                        to="/login"
+                        class="button is-outlined"
+                        :class="theme.secondary.class"
+                    >{{ t('navbar.login') }}
+                    </router-link>
+
+                    <router-link
+                        to="/register"
+                        class="button is-outlined"
+                        :class="theme.secondary.class"
+                    >{{ t('navbar.register') }}
+                    </router-link>
+                </div>
             </div>
-            <div style="display: flex; justify-content: space-around;">
-                <a :class="['button', theme.secondary.class]">Sign-in</a>
-                <a :class="['button', theme.secondary.class]">Create New Account</a>
-            </div>
-        </div>
+        </div><!-- // .main-hero-left -->
+
+        <div class="main-hero-right">
+<!--
+            <h3>Login</h3>
+            <login-panel
+                @update:success="loginSuccess"
+                @update:failed="loginFailed"
+            ></login-panel>
+-->
+        </div><!-- // .main-hero-right -->
 
     </div>
     </div>
@@ -42,17 +68,34 @@
 </template>
 
 <script>
+import core from '../mixins/core.js';
+//import loginPanel from './user/login/login-panel.vue';
+
 export default {
+//    components: { loginPanel },
+
+    mixins: [ core ],
+
     computed: {
         theme: function() {
             return this.$store.state.app.theme;
         }
-    }
+    },
+
+//    methods: {
+//        loginSuccess() {
+//
+//        },
+//
+//        loginFailed() {
+//
+//        }
+//    }
 }
 </script>
 
 <style lang="scss" scoped>
-.contents {
-    /* display: flex; */
+.main-hero-right {
+    width: 50%;
 }
 </style>
