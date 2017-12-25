a39910c51cf30809e7a6f9fa4741e9a52a5e1b49
@ontheroad_jp <dev@ontheroad.jp>
2017-12-26 07:07:00 +0900

    [Modify] minor issue

--- laravel/resources/assets/js/src/pages/partials/footer/index.vue
+++ laravel/resources/assets/js/src/pages/partials/footer/index.vue
@@ -1,18 +1,36 @@
 <template>
     <footer class="footer">
         <div class="container">
-            <div class="content has-text-centered">
-                <p>
-                <strong>Nuts Days</strong> by <a href="//jgthms.com">Nuts, LLC</a>. The source code is licensed
-                <a href="//opensource.org/licenses/mit-license.php">MIT</a>. The website content
-                is licensed <a href="//creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+            <div class="content has-text-centered thin">
+                <p>Copyright (C) 2017 Nuts, LLC. All Rights Reserved.</p>
+                <p v-if="isMIT">
+                    The source code is licensed
+                    <a href="//opensource.org/licenses/mit-license.php">MIT</a>.
                 </p>
-                <p>
-                <a class="icon" href="//github.com/jgthms/bulma">
-                    <i class="fa fa-github"></i>
-                </a>
+
+                <p v-if="isCCLANS">
+                    The website content is licensed
+                    <a href="//creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+                </p>
+
+                <p v-if="github !== ''">
+                    <a class="icon" :href="github">
+                        <i class="fa fa-github"></i>
+                    </a>
                 </p>
             </div>
         </div>
     </footer>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            isMIT: false,
+            isCCLANS: false,
+            github: ''
+        }
+    }
+}
+</script>
