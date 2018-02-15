e3adbf1926f1ab2ae6fcb2aa2704880967cb1123
@ontheroad_jp <dev@ontheroad.jp>
2018-02-15 23:28:50 +0900

    [Clean] remove codes commented out

--- laravel/resources/assets/js/src/store/modules/userCalendar/insert.js
+++ laravel/resources/assets/js/src/store/modules/userCalendar/insert.js
@@ -19,13 +19,11 @@ export default {
 
     actions: {
         setInsertValue( { commit }, { key, value } ) {
-//            commit('setInsertValue', { key, value });
             commit(SET_INSERT_VALUE, { key, value });
         },
 
         insert( { dispatch, commit, state }, { notify, successCb, failedCb } ) {
             u.clog('insert()');
-//            commit('isLoading', true);
             commit(IS_LOADING, true);
 
             const url = '/api/v1/calendar';
@@ -53,14 +51,12 @@ export default {
                         }, { root: true });
                     }
 
-//                    commit('isLoading', false);
                     commit(IS_LOADING, false);
 
                     if( typeof successCb === 'function' ) {
                         successCb();
                     }
 
-//                    commit('reset');
                     commit(RESET);
                 })
 
@@ -74,36 +70,30 @@ export default {
                         }, { root: true });
                     }
 
-//                    commit('isLoading', false);
                     commit(IS_LOADING, false);
 
                     if( typeof failedCb === 'function' ) {
                         failedCb();
                     }
 
-//                    commit('reset');
                     commit(RESET);
                 });
         },
 
         reset( { commit } ) {
-//            commit('reset');
             commit(RESET);
         }
     },
 
     mutations: {
-//        isLoading( state, value ) {
         [IS_LOADING]( state, value ) {
             state.isLoading = value;
         },
 
-//        setInsertValue( state, { key, value } ) {
         [SET_INSERT_VALUE]( state, { key, value } ) {
             state.insertValues[key] = value;
         },
 
-//        reset( state ) {
         [RESET]( state ) {
             state.isLoading = false;
             Object.keys(state.insertValues).forEach(function(key) {
