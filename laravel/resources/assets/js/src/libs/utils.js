7be527e73b191fe1da04bbdc72483b52f46d1b94
@ontheroad_jp <dev@ontheroad.jp>
2018-02-15 19:26:48 +0900

    [Add] function isDebug

--- laravel/resources/assets/js/src/libs/utils.js
+++ laravel/resources/assets/js/src/libs/utils.js
@@ -2,6 +2,10 @@ export const utils = {
 
     debug: true,
 
+    isDebug: function() {
+        return this.debug;
+    },
+
     clog: function(message) {
         if(this.debug) console.log('%s', message);
     },
